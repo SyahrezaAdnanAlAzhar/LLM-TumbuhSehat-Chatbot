e4eac4efd9330ef8c856f741205669045d774d20
SyahrezaAdnanAlAzhar <syahreza.adnan29@gmail.com>
2025-03-27 21:07:30 +0700

    change model

--- main.py
+++ main.py
@@ -1,22 +1,30 @@
 import streamlit as st
 import google.generativeai as genai
 
-# Konfigurasi API Key (Ganti dengan API key-mu)
 API_KEY = st.secrets["GEMINI_API_KEY"]
 genai.configure(api_key=API_KEY)
 
 # Inisialisasi model Gemini
-model = genai.GenerativeModel("gemini-2.0-flash")
+generation_config = {
+  "temperature": 1,
+  "top_p": 0.95,
+  "top_k": 40,
+  "max_output_tokens": 8192,
+  "response_mime_type": "text/plain",
+}
 
-# Fungsi untuk mendapatkan respons dari Gemini
+model = genai.GenerativeModel(
+  model_name="tunedModels/tumbuhsehatfinetunellm-cwr8817ckikt",
+  generation_config=generation_config,
+)
+
 def get_gemini_response(user_input):
     response = model.generate_content(user_input)
     return response.text if response else "Maaf, terjadi kesalahan."
 
 # Streamlit UI
-st.title("Chatbot Gemini dengan Streamlit")
+st.title("TumbuhSehat Chatbot")
 
-# Inisialisasi sesi
 if "messages" not in st.session_state:
     st.session_state.messages = []
     # Kirim pesan "Halo" secara otomatis saat pertama kali dibuka
@@ -25,21 +33,17 @@ if "messages" not in st.session_state:
     st.session_state.messages.append({"role": "user", "content": first_message})
     st.session_state.messages.append({"role": "bot", "content": first_response})
 
-# Tampilkan semua pesan
 for msg in st.session_state.messages:
     if msg["role"] == "user":
         st.chat_message("user").markdown(msg["content"])
     else:
         st.chat_message("assistant").markdown(msg["content"])
 
-# Input pengguna
 user_input = st.chat_input("Ketik pesan...")
 if user_input:
-    # Tampilkan pesan pengguna
     st.chat_message("user").markdown(user_input)
     st.session_state.messages.append({"role": "user", "content": user_input})
     
-    # Dapatkan respons dari Gemini
     response = get_gemini_response(user_input)
     st.chat_message("assistant").markdown(response)
     st.session_state.messages.append({"role": "bot", "content": response})
